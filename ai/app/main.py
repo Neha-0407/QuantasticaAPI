f9a713f0de047d0180d2d594806ebff22997af41
simran <simranhora912@gmail.com>
2025-07-26 11:11:05 +0530

    updating endpoints

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from app.api.routes import prompt
+app = FastAPI()
+
+from app.api.routes import prompt, testdata
 from app.api.sessions import session
 
 
-app = FastAPI()
-
-# app.include_router(voice.router, prefix="/voice")
-# app.include_router(finance.router, prefix="/finance")
-# app.include_router(group.router, prefix="/group")
+#app.include_router(voice.router, prefix="/voice")
+#app.include_router(finance.router, prefix="/finance")
+#app.include_router(group.router, prefix="/group")
 app.include_router(session.router, prefix="/sessions")
 app.include_router(prompt.router, prefix="/prompt")
+app.include_router(testdata.router, prefix="/data")
+
